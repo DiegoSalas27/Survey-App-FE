6fff5800e8f44a4885f492dc15c17c2e412299ba
dominicsc2 <dominicsc2hs@gmail.com>
2022-03-06 14:18:08 -0600

    feat: ensure RemoteAuthenticationUseCase calls httpPostClient with correct URL

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,16 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: [
-    '<rootDir>/src/**/*.(ts,tsx)'
-  ],
+  collectCoverageFrom: ['<rootDir>/src/**/*.(ts,tsx)'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
+  moduleNameMapper: {
+    // '@presentation/(.*)$': '<rootDir>/src/presentation/$1',
+    // '@infrastructure/(.*)$': '<rootDir>/src/infrastructure/$1',
+    '@data/(.*)$': '<rootDir>/src/data/$1',
+    '@domain/(.*)$': '<rootDir>/src/domain/$1'
+    // '@main/(.*)$': '<rootDir>/src/main/$1',
+    // '@validation/(.*)$': '<rootDir>/src/validation/$1'
+  },
   transform: {
     '.+\\.ts$': 'ts-jest'
   }
