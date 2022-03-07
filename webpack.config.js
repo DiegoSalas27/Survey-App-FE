87cfe4265adbb2c6d00c80c560f474b37e333a99
dominicsc2 <dominicsc2hs@gmail.com>
2022-03-07 15:01:09 -0600

    chore: config sass

--- webpack.config.js
+++ webpack.config.js
@@ -10,12 +10,31 @@ module.exports = {
     filename: 'bundle.js'
   },
   resolve: {
-    extensions: ['.ts', '.tsx', '.js'],
+    extensions: ['.ts', '.tsx', '.js', 'scss'],
     alias: {
       '@domain': path.join(__dirname, 'src/domain'),
       '@data': path.join(__dirname, 'src/data'),
       '@infrastructure': path.join(__dirname, 'src/infrastructure'),
       '@main': path.join(__dirname, 'src/main')
+    },
+    module: {
+      rules: [{
+        test: /\.ts(x?)$/,
+        loader: 'ts-loader',
+        exclude: /node_modules/
+      }, {
+        test: /\.scss$/,
+        use: [{
+          loader: 'style-loader'
+        }, {
+          loader: 'css-loader',
+          options: {
+            modules: true
+          }
+        }, {
+          loader: 'sass-loader'
+        }]
+      }]
     },
     devServer: {
       // generate the bundle file in memory and runs the server
