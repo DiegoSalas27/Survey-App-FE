6a314b2b7f240f66388f79de39e4ce6d28fc3590
dominicsc2 <dominicsc2hs@gmail.com>
2022-03-08 12:08:32 -0600

    test: ensure Login Component adds accessToken to localstorage on success

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -1,9 +1,10 @@
+import React from 'react'
+import Login from './login'
+import 'jest-localstorage-mock'
 import { InvalidCredentialsError } from '@domain/errors'
 import faker from '@faker-js/faker'
 import { ValidationStub, AuthenticationSpy } from '@presentation/test'
 import { cleanup, fireEvent, render, RenderResult, waitFor } from '@testing-library/react'
-import React from 'react'
-import Login from './login'
 
 type SutTypes = {
   sut: RenderResult
@@ -55,6 +56,9 @@ const simulateStatusForField = (sut: RenderResult, fieldName: string, validation
 
 describe('Login Component', () => {
   afterEach(cleanup)
+  beforeEach(() => {
+    localStorage.clear()
+  })
 
   test('Should start with initial state', () => {
     const validationError = faker.random.words()
@@ -144,5 +148,12 @@ describe('Login Component', () => {
     const mainError = sut.getByTestId('main-error')
     expect(mainError.textContent).toBe(error.message)
     expect(errorWrap.childElementCount).toBe(1)
+  })
+
+  test('Should add accessToken to localstorage if Authentication succeeds', async () => {
+    const { sut, authenticationSpy } = makeSut()
+    simulateValidSubmit(sut)
+    await waitFor(() => sut.getByTestId('form'))
+    expect(localStorage.setItem).toHaveBeenCalledWith('accessToken', authenticationSpy.account.accessToken)
   })
 })
