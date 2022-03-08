4c7ff5d482b20dd5fb38d48f3f5cebd8b8d82103
dominicsc2 <dominicsc2hs@gmail.com>
2022-03-08 10:32:14 -0600

    test: ensure Login Component enables submit button if form is valid

--- src/presentation/pages/login/login.spec.tsx
+++ src/presentation/pages/login/login.spec.tsx
@@ -74,4 +74,15 @@ describe('Login Component', () => {
     expect(passwordStatus.title).toBe('It\'s all good')
     expect(passwordStatus.textContent).toBe('😃')
   })
+
+  test('Should enable submit button if form is valid', () => {
+    const { sut, validationStub: validationSpy } = makeSut()
+    validationSpy.errorMessage = null
+    const emailInput = sut.getByTestId('email')
+    fireEvent.input(emailInput, { target: { value: faker.internet.email() } })
+    const passwordInput = sut.getByTestId('password')
+    fireEvent.input(passwordInput, { target: { value: faker.internet.password() } })
+    const submitButton = sut.getByTestId('submit') as HTMLButtonElement
+    expect(submitButton.disabled).toBe(false)
+  })
 })
