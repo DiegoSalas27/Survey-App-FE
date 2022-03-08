f271ff80fee98d9d6a8f31cd443bbdaf59b16768
dominicsc2 <dominicsc2hs@gmail.com>
2022-03-08 10:15:52 -0600

    feat: ensure Login Component shows valid password state if Validation succeeds

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -5,8 +5,8 @@ import Context from '@presentation/context/form/form-context'
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 const Input: React.FC<Props> = (props: Props) => {
-  const { state, setState } = useContext(Context) as any
-  const error = state[`${props.name!}Error`]
+  const { state, setState } = useContext(Context)
+  const error = state[`${props.name}Error`]
 
   const enableInput = (e: React.FocusEvent<HTMLInputElement>): void => {
     e.target.readOnly = false
@@ -20,17 +20,17 @@ const Input: React.FC<Props> = (props: Props) => {
   }
 
   const getStatus = (): string => {
-    return '😢'
+    return error ? '😢' : '😃'
   }
 
   const getTitle = (): string => {
-    return error
+    return error || 'It\'s all good'
   }
 
   return (
     <div className={Styles.inputWrap}>
       <input {...props} data-testid={props.name} readOnly onFocus={enableInput} onChange={handleChange} />
-      <span data-testid={`${props.name!}-status`} title={getTitle()} className={Styles.status}>
+      <span data-testid={`${props.name}-status`} title={getTitle()} className={Styles.status}>
         {getStatus()}
       </span>
     </div>
