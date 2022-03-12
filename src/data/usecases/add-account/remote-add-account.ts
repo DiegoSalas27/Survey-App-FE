3b32d709bf80001bb2f16c9daf1719a1de05af80
dominicsc2 <dominicsc2hs@gmail.com>
2022-03-12 08:50:11 -0600

    feat: ensure RemoteAddAccount throws UnexpectedError if HttpPostClient returns 400

--- src/data/usecases/add-account/remote-add-account.ts
+++ src/data/usecases/add-account/remote-add-account.ts
@@ -1,5 +1,5 @@
 import { HttpPostClient, HttpStatusCode } from '@data/protocols/http'
-import { EmailInUseError } from '@domain/errors'
+import { EmailInUseError, UnexpectedError } from '@domain/errors'
 import { AddAccount, AddAccountParams } from '@domain/usecases'
 import { AccountModel } from '../authentication/remote-authentication-protocols'
 
@@ -16,7 +16,8 @@ export class RemoteAddAccount implements AddAccount {
         return httpResponse.body
       case HttpStatusCode.forbidden:
         throw new EmailInUseError()
-      default: return null
+      default:
+        throw new UnexpectedError()
     }
   }
 }
