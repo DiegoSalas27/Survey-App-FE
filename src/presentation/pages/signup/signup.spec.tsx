bfae91695578e5766cca019ff481fab941f26eaa
dominicsc2 <dominicsc2hs@gmail.com>
2022-03-12 11:10:29 -0600

    test: ensure Login component shows valid fieldsw state if Validation succeeds

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -64,4 +64,28 @@ describe('Signup Component', () => {
     Helper.populateField(sut, 'passwordConfirm')
     Helper.testStatusForField(sut, 'passwordConfirm', validationError)
   })
+
+  test('Should show valid name state if Validation succeeds', () => {
+    const { sut } = makeSut()
+    Helper.populateField(sut, 'name')
+    Helper.testStatusForField(sut, 'name')
+  })
+
+  test('Should show valid email state if Validation succeeds', () => {
+    const { sut } = makeSut()
+    Helper.populateField(sut, 'email')
+    Helper.testStatusForField(sut, 'email')
+  })
+
+  test('Should show valid password state if Validation succeeds', () => {
+    const { sut } = makeSut()
+    Helper.populateField(sut, 'password')
+    Helper.testStatusForField(sut, 'password')
+  })
+
+  test('Should show valid password confirmation state if Validation succeeds', () => {
+    const { sut } = makeSut()
+    Helper.populateField(sut, 'passwordConfirm')
+    Helper.testStatusForField(sut, 'passwordConfirm')
+  })
 })
