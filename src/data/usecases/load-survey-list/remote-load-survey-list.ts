ea2c5db066f6b6efb7c31bd1a3df9790abbfd968
dominicsc2 <dominicsc2hs@gmail.com>
2022-03-13 13:48:43 -0500

    feat: ensure RemoteLoadSurveyList thros UnexpectedError if HttpGetClient returns 403

--- src/data/usecases/load-survey-list/remote-load-survey-list.ts
+++ src/data/usecases/load-survey-list/remote-load-survey-list.ts
@@ -1,12 +1,22 @@
-import { HttpGetClient } from '@data/protocols/http'
+import { HttpGetClient, HttpStatusCode } from '@data/protocols/http'
+import { EmailInUseError } from '@domain/errors'
 import { SurveyModel } from '@domain/models'
 import { LoadSurveyList } from '@domain/usecases'
+import { UnexpectedError } from '../authentication/remote-authentication-protocols'
 
 export class RemoteLoadSurveyList implements LoadSurveyList {
-  constructor(private readonly url: string, private readonly httpGetClient: HttpGetClient) {}
+  constructor(
+    private readonly url: string,
+    private readonly httpGetClient: HttpGetClient<SurveyModel[]>
+  ) {}
 
-  async loadAll(): Promise<SurveyModel[]> {
-    await this.httpGetClient.get({ url: this.url })
-    return await Promise.resolve(null)
+  async loadAll(): Promise<any> {
+    const httpResponse = await this.httpGetClient.get({ url: this.url })
+    switch (httpResponse.statusCode) {
+      case HttpStatusCode.ok:
+        break
+      default:
+        throw new UnexpectedError()
+    }
   }
 }
