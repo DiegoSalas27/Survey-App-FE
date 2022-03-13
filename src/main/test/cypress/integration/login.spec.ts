d25b15d3e3ee895106d79da55d3688b605184fed
dominicsc2 <dominicsc2hs@gmail.com>
2022-03-13 06:05:57 -0500

    refactor: change test to use faker

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -36,39 +36,110 @@ describe('login', () => {
   it('Should present valid state if form is valid', () => {
     cy.getByTestId('email').focus().type(faker.internet.email())
     cy.getByTestId('email-status')
-      .should('have.attr', 'title', 'It\'s all good')
+      .should('have.attr', 'title', "It's all good")
       .should('contain.text', '😃')
     cy.getByTestId('password').focus().type(faker.random.alphaNumeric(6))
     cy.getByTestId('password-status')
-      .should('have.attr', 'title', 'It\'s all good')
+      .should('have.attr', 'title', "It's all good")
       .should('contain.text', '😃')
     cy.getByTestId('submit').should('not.have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
   })
 
-  it('Should present error if invalid credentials are provided', () => {
+  it('Should present error InvalidCredentialsError on 401', () => {
+    // url from api can be different from the local one. We want to use regex for it to find a word.
+    cy.intercept('POST', /login/, req => {
+      req.reply(res => {
+        res.send(401, res.body)
+      })
+    }).as('loginRequest')
+
     cy.getByTestId('email').focus().type(faker.internet.email())
     cy.getByTestId('password').focus().type(faker.random.alphaNumeric(6))
     cy.getByTestId('submit').click()
     cy.getByTestId('error-wrap')
-      .getByTestId('spinner').should('exist')
-      .getByTestId('main-error').should('not.exist')
-      .getByTestId('spinner').should('not.exist')
+      .getByTestId('spinner')
+      .should('exist')
       .getByTestId('main-error')
-        .should('exist')
-        .should('contain.text', 'Invalid Credentials')
-    cy.url().should('eq', `${baseUrl}/login`)
+      .should('not.exist')
+
+    cy.wait('@loginRequest').then(res => {
+      cy.getByTestId('spinner').should('not.exist')
+      cy.getByTestId('main-error').should('exist').should('contain.text', 'Invalid Credentials')
+      cy.url().should('eq', `${baseUrl}/login`)
+    })
+  })
+
+  it('Should present error UnexpectedError on 400', () => {
+    // url from api can be different from the local one. We want to use regex for it to find a word.
+    cy.intercept('POST', /login/, req => {
+      req.reply(res => {
+        res.send(400, res.body)
+      })
+    }).as('loginRequest')
+
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(6))
+    cy.getByTestId('submit').click()
+    cy.getByTestId('error-wrap')
+      .getByTestId('spinner')
+      .should('exist')
+      .getByTestId('main-error')
+      .should('not.exist')
+
+    cy.wait('@loginRequest').then(res => {
+      cy.getByTestId('spinner').should('not.exist')
+      cy.getByTestId('main-error').should('exist').should('contain.text', 'An error has occurred. Please try again.')
+      cy.url().should('eq', `${baseUrl}/login`)
+    })
+  })
+
+  it('Should present error UnexpectedErrorif invalid data is returned', () => {
+    cy.intercept('POST', /login/, req => {
+      req.reply(res => {
+        res.send(200, {
+          ivalidProperty: faker.datatype.uuid()
+        })
+      })
+    }).as('loginRequest')
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(6))
+    cy.getByTestId('submit').click()
+    cy.getByTestId('error-wrap')
+      .getByTestId('spinner')
+      .should('exist')
+      .getByTestId('main-error')
+      .should('not.exist')
+
+    cy.wait('@loginRequest').then(res => {
+      cy.getByTestId('spinner').should('not.exist')
+      cy.getByTestId('main-error').should('exist').should('contain.text', 'An error has occurred. Please try again.')
+      cy.url().should('eq', `${baseUrl}/login`)
+    })
   })
 
   it('Should present save accessToken if valid credentials are provided', () => {
-    cy.getByTestId('email').focus().type('mongolo@gmail.com')
-    cy.getByTestId('password').focus().type('123456')
+    cy.intercept('POST', /login/, req => {
+      req.reply(res => {
+        res.send(200, {
+          accessToken: faker.datatype.uuid()
+        })
+      })
+    }).as('loginRequest')
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(6))
     cy.getByTestId('submit').click()
     cy.getByTestId('error-wrap')
-      .getByTestId('spinner').should('exist')
-      .getByTestId('main-error').should('not.exist')
-      .getByTestId('spinner').should('not.exist')
-    cy.url().should('eq', `${baseUrl}/`)
-    cy.window().then(window => assert.isOk(window.localStorage.getItem('accessToken')))
+      .getByTestId('spinner')
+      .should('exist')
+      .getByTestId('main-error')
+      .should('not.exist')
+
+    cy.wait('@loginRequest').then(res => {
+      cy.getByTestId('spinner').should('not.exist')
+      cy.getByTestId('main-error').should('not.exist')
+      cy.url().should('eq', `${baseUrl}/`)
+      cy.window().then(window => assert.isOk(window.localStorage.getItem('accessToken')))
+    })
   })
 })
