4884c31400b33b5589d9b12ca9fc4b0f8fb54201
dominicsc2 <dominicsc2hs@gmail.com>
2022-03-15 22:45:18 -0500

    feat: ensure SurveyItem component renders icon and data with correct values

--- src/presentation/pages/survey-list/components/survey-item/survey-item.tsx
+++ src/presentation/pages/survey-list/components/survey-item/survey-item.tsx
@@ -8,14 +8,15 @@ type Props = {
 }
 
 const SurveyItem: React.FC<Props> = ({ survey }) => {
+  const iconName = survey.didAnswer ? IconName.thumbUp : IconName.thumbDown
   return (
     <div className={Styles.surveyItemWrap}>
       <li>
         <div className={Styles.surveyContent}>
-          <Icon className={Styles.iconWrap} iconName={IconName.thumbUp} />
+          <Icon className={Styles.iconWrap} iconName={iconName} />
           <time>
             <span data-testid="day" className={Styles.day}>
-              {survey.date.getDate()}
+              {survey.date.getDate().toString().padStart(2, '0')}
             </span>
             <span data-testid="month" className={Styles.month}>
               {survey.date.toLocaleString('es-PE', { month: 'short' }).slice(0, -1)}
