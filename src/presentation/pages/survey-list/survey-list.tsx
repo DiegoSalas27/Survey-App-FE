15e0eb017c9b5c393c73fb04f902a0e7e674341d
dominicsc2 <dominicsc2hs@gmail.com>
2022-03-15 21:38:20 -0500

    feat: ensuure SurveyList calls LoadSurveyList once

--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,10 +1,20 @@
+import { LoadSurveyList } from '@domain/usecases'
 import { Footer } from '@presentation/components'
 import Header from '@presentation/components/header/header'
-import React from 'react'
+import React, { useEffect } from 'react'
 import { SurveyItemEmpty } from './components'
 import Styles from './survey-list-styles.scss'
 
-const SurveyList: React.FC = () => {
+type Props = {
+  loadSurveyList: LoadSurveyList
+}
+
+const SurveyList: React.FC<Props> = ({ loadSurveyList }) => {
+  useEffect(() => {
+    (async () => {
+      loadSurveyList.loadAll()
+    })()
+  }, [])
   return (
     <div className={Styles.surveyListWrap}>
       <Header />
