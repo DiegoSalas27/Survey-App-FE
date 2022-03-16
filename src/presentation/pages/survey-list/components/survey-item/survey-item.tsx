5a28fe4380c4e7fbf254c2ab7c379d79e0531fbc
dominicsc2 <dominicsc2hs@gmail.com>
2022-03-15 23:17:34 -0500

    feat: ensure SurveyList component renders SurveyItems on success

--- src/presentation/pages/survey-list/components/survey-item/survey-item.tsx
+++ src/presentation/pages/survey-list/components/survey-item/survey-item.tsx
@@ -10,26 +10,24 @@ type Props = {
 const SurveyItem: React.FC<Props> = ({ survey }) => {
   const iconName = survey.didAnswer ? IconName.thumbUp : IconName.thumbDown
   return (
-    <div className={Styles.surveyItemWrap}>
-      <li>
-        <div className={Styles.surveyContent}>
-          <Icon className={Styles.iconWrap} iconName={iconName} />
-          <time>
-            <span data-testid="day" className={Styles.day}>
-              {survey.date.getDate().toString().padStart(2, '0')}
-            </span>
-            <span data-testid="month" className={Styles.month}>
-              {survey.date.toLocaleString('es-PE', { month: 'short' }).slice(0, -1)}
-            </span>
-            <span data-testid="year" className={Styles.year}>
-              {survey.date.getFullYear()}
-            </span>
-          </time>
-          <p data-testid="question">{survey.question}</p>
-        </div>
-        <footer>View result</footer>
-      </li>
-    </div>
+    <li className={Styles.surveyItemWrap}>
+      <div className={Styles.surveyContent}>
+        <Icon className={Styles.iconWrap} iconName={iconName} />
+        <time>
+          <span data-testid="day" className={Styles.day}>
+            {survey.date.getDate().toString().padStart(2, '0')}
+          </span>
+          <span data-testid="month" className={Styles.month}>
+            {survey.date.toLocaleString('es-PE', { month: 'short' }).slice(0, -1)}
+          </span>
+          <span data-testid="year" className={Styles.year}>
+            {survey.date.getFullYear()}
+          </span>
+        </time>
+        <p data-testid="question">{survey.question}</p>
+      </div>
+      <footer>View result</footer>
+    </li>
   )
 }
 
--- src/presentation/pages/survey-list/survey-list.tsx
+++ src/presentation/pages/survey-list/survey-list.tsx
@@ -1,8 +1,9 @@
+import { SurveyModel } from '@domain/models'
 import { LoadSurveyList } from '@domain/usecases'
 import { Footer } from '@presentation/components'
 import Header from '@presentation/components/header/header'
-import React, { useEffect } from 'react'
-import { SurveyItemEmpty } from './components'
+import React, { useEffect, useState } from 'react'
+import { SurveyItem, SurveyItemEmpty } from './components'
 import Styles from './survey-list-styles.scss'
 
 type Props = {
@@ -10,18 +11,27 @@ type Props = {
 }
 
 const SurveyList: React.FC<Props> = ({ loadSurveyList }) => {
+  const [state, setState] = useState({
+    surveys: [] as SurveyModel[]
+  })
+
   useEffect(() => {
-    (async () => {
-      loadSurveyList.loadAll()
-    })()
+    loadSurveyList.loadAll().then(surveys => setState({ surveys }))
   }, [])
+
   return (
     <div className={Styles.surveyListWrap}>
       <Header />
       <div className={Styles.contentWrap}>
         <h2>Polls</h2>
         <ul data-testid="survey-list">
-          <SurveyItemEmpty />
+          {state.surveys.length ? (
+            state.surveys.map((survey: SurveyModel) => (
+              <SurveyItem key={survey.id} survey={survey} />
+            ))
+          ) : (
+            <SurveyItemEmpty />
+          )}
         </ul>
       </div>
       <Footer />
