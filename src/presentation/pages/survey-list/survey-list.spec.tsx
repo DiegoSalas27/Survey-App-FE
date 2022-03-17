5a63646c9e0a7c524040ff6f2a8d7252b48d73bd
dominicsc2 <dominicsc2hs@gmail.com>
2022-03-16 21:49:10 -0500

    test: ensure SurveyList calls LoadSurveyList on reload

--- src/presentation/pages/survey-list/survey-list.spec.tsx
+++ src/presentation/pages/survey-list/survey-list.spec.tsx
@@ -3,8 +3,9 @@ import { SurveyModel } from '@domain/models'
 import { mockSurveyListModel } from '@domain/test'
 import { LoadSurveyList } from '@domain/usecases'
 import { SurveyList } from '@presentation/pages'
-import { render, screen, waitFor } from '@testing-library/react'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import React from 'react'
+import error from './components/error/error'
 
 class LoadSurveyListSpy implements LoadSurveyList {
   callsCount = 0
@@ -59,5 +60,15 @@ describe('SurveyList Component', () => {
     await waitFor(() => screen.getByRole('heading'))
     expect(screen.queryByTestId('survey-list')).not.toBeInTheDocument()
     expect(screen.getByTestId('error')).toHaveTextContent(error.message)
+  })
+
+  test('Should call LoadSurveyList on reload', async () => {
+    const loadSurveyListSpy = new LoadSurveyListSpy()
+    jest.spyOn(loadSurveyListSpy, 'loadAll').mockRejectedValueOnce(new UnexpectedError())
+    makeSut(loadSurveyListSpy)
+    await waitFor(() => screen.getByRole('heading'))
+    fireEvent.click(screen.getByTestId('reload'))
+    expect(loadSurveyListSpy.callsCount).toBe(1)
+    await waitFor(() => screen.getByRole('heading'))
   })
 })
