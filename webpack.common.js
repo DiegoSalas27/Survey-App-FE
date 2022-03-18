9dbf3ab340ab752bf4d17a784d091b3a419aba11
dominicsc2 <dominicsc2hs@gmail.com>
2022-03-17 23:31:38 -0500

    chore: add webpack prod and dev configs

---
+++ webpack.common.js
@@ -0,0 +1,25 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: './src/main/index.tsx',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    publicPath: '/dist',
+    filename: 'main-bundle-[hash].js'
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', 'scss'],
+    alias: {
+      '@domain': path.join(__dirname, 'src/domain'),
+      '@data': path.join(__dirname, 'src/data'),
+      '@infrastructure': path.join(__dirname, 'src/infrastructure'),
+      '@presentation': path.join(__dirname, 'src/presentation'),
+      '@main': path.join(__dirname, 'src/main'),
+      '@validation': path.join(__dirname, 'src/validation')
+    }
+  },
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
+}
