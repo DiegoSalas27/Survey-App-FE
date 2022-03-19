fd7b1d16a1b0def04074b7bc7b4e489af44329d2
dominicsc2 <dominicsc2hs@gmail.com>
2022-03-18 20:13:43 -0500

    feat: ensure RemoteLoadSurveyResult returns a SurveyResult on 200

--- src/data/usecases/load-survey-result/remote-load-survey-result.ts
+++ src/data/usecases/load-survey-result/remote-load-survey-result.ts
@@ -2,21 +2,37 @@ import { HttpGetClient, HttpStatusCode } from '@data/protocols/http'
 import { AccessDeniedError, UnexpectedError } from '@domain/errors'
 import { LoadSurveyResult } from '@domain/usecases'
 
-export class RemoteLoadSurveyResult {
+export class RemoteLoadSurveyResult implements LoadSurveyResult {
   constructor(
     private readonly url: string,
-    private readonly httpGetClient: HttpGetClient<LoadSurveyResult.Model[]>
+    private readonly httpGetClient: HttpGetClient<LoadSurveyResult.Model>
   ) {}
 
-  async load(): Promise<void> {
+  async load(): Promise<LoadSurveyResult.Model> {
     const httpResponse = await this.httpGetClient.get({ url: this.url })
+    const remoteSurveyResult = httpResponse.body
     switch (httpResponse.statusCode) {
       case HttpStatusCode.ok:
-        break
+        return remoteSurveyResult
+          ? Object.assign({}, remoteSurveyResult, { date: new Date(remoteSurveyResult.date) })
+          : null
       case HttpStatusCode.forbidden:
         throw new AccessDeniedError()
       default:
         throw new UnexpectedError()
     }
+  }
+}
+
+export namespace RemoteLoadSurveyResult {
+  export type Model = {
+    question: string
+    answers: Array<{
+      image?: string
+      answer: string
+      count: number
+      percent: number
+    }>
+    date: string
   }
 }
