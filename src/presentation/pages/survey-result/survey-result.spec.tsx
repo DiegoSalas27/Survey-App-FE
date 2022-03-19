497dfe9b9056a27e8c6dfc7c9f2fd9c95038031e
dominicsc2 <dominicsc2hs@gmail.com>
2022-03-19 18:40:54 -0500

    test: ensure SurveyResult component presents correct initial state

---
+++ src/presentation/pages/survey-result/survey-result.spec.tsx
@@ -0,0 +1,25 @@
+import { mockAccountModel } from '@domain/test'
+import { ApiContext } from '@presentation/context'
+import { render, screen } from '@testing-library/react'
+import React from 'react'
+import SurveyResult from './survey-result'
+
+const makeSut = () => {
+  render(
+    <ApiContext.Provider
+      value={{ setCurrentAccount: jest.fn(), getCurrentAccount: () => mockAccountModel() }}
+    >
+      <SurveyResult />
+    </ApiContext.Provider>
+  )
+}
+
+describe('SurveyResult Component', () => {
+  test('Should present correct initial state', () => {
+    makeSut()
+    const surveyResult = screen.getByTestId('survey-result')
+    expect(surveyResult.childElementCount).toBe(0)
+    expect(screen.queryByTestId('error')).not.toBeInTheDocument()
+    expect(screen.queryByTestId('loading')).not.toBeInTheDocument()
+  })
+})
