45e828d6390480177bcdc33a6652e07bfed3ee07
dominicsc2 <dominicsc2hs@gmail.com>
2022-03-21 15:16:37 -0500

    feat: add SaveSurveyResult usecase

--- src/domain/usecases/load-survey-result.ts
+++ src/domain/usecases/load-survey-result.ts
@@ -1,17 +1,9 @@
+import { SurveyResultAnswerModel, SurveyResultModel } from '@domain/models'
+
 export interface LoadSurveyResult {
   load: () => Promise<LoadSurveyResult.Model>
 }
 
 export namespace LoadSurveyResult {
-  export type Model = {
-    question: string
-    answers: Array<{
-      image?: string
-      answer: string
-      count: number
-      percent: number
-      isCurrentAccountAnswer: boolean
-    }>
-    date: Date
-  }
+  export type Model = SurveyResultModel
 }
