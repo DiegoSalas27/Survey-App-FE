a6fc941be9b774cf5cffa1f2b3eb39a1adb67360
dominicsc2 <dominicsc2hs@gmail.com>
2022-03-20 20:11:12 -0500

    test: ensure SurveyResult present survey result on success

--- src/main/test/cypress/integration/survey-result.spec.ts
+++ src/main/test/cypress/integration/survey-result.spec.ts
@@ -37,4 +37,24 @@ describe('SurveyResult', () => {
     mockAccessDeniedError()
     testUrl('/login')
   })
+
+  it('Should present survey result', () => {
+    mockOk()
+    cy.getByTestId('question').should('have.text', 'Question 2')
+    cy.getByTestId('day').should('have.text', '20')
+    cy.getByTestId('month').should('have.text', 'Oct')
+    cy.getByTestId('year').should('have.text', '2020')
+    cy.wait('@request').then(res => {
+      cy.get('li:nth-child(1)').then(li => {
+        assert.equal(li.find('[data-testid="answer"]').text(), 'any_answer')
+        assert.equal(li.find('[data-testid="percent"]').text(), '70%')
+        assert.equal(li.find('[data-testid="image"]').attr('src'), 'any_image')
+      })
+      cy.get('li:nth-child(2)').then(li => {
+        assert.equal(li.find('[data-testid="answer"]').text(), 'any_answer_2')
+        assert.equal(li.find('[data-testid="percent"]').text(), '30%')
+        assert.notExists(li.find('[data-testid="image"]'))
+      })
+    })
+  })
 })
